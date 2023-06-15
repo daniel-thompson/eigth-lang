5c60dfa7295e8404a34e0e43bbb3df1d8b76bd21
Daniel Thompson <daniel@redfelineninja.org.uk>
2023-06-15 23:06:26 +0100

    op: Make the op functions uniform

--- src/op.c
+++ src/op.c
@@ -2,22 +2,22 @@
 
 #include "eigth.h"
 
-static reg_t add(reg_t _, reg_t a, reg_t b)
+static reg_t op_add(reg_t _, reg_t a, reg_t b)
 {
 	return a + b;
 }
 
-static reg_t x_alloc(reg_t _, reg_t sz)
+static reg_t op_alloc(reg_t _, reg_t sz)
 {
 	return (reg_t) (uintptr_t) alloc(sz);
 }
 
-static reg_t and(reg_t _, reg_t a, reg_t b)
+static reg_t op_and(reg_t _, reg_t a, reg_t b)
 {
 	return a & b;
 }
 
-static reg_t x_assert(reg_t a, reg_t b)
+static reg_t op_assert(reg_t a, reg_t b)
 {
 	if (a != b)
 		die("Assertion failed: 0x%x != 0x%x", a, b);
@@ -25,72 +25,73 @@ static reg_t x_assert(reg_t a, reg_t b)
 	return a;
 }
 
-static reg_t define(void)
+
+static reg_t op_define(void)
 {
 	parse_define();
 	return 0;
 }
 
-static reg_t x_disassemble(void)
+static reg_t op_disassemble(void)
 {
 	symtab_disassemble();
 	return 0;
 }
 
-static reg_t dump(void)
+static reg_t op_dump(void)
 {
 	dbg_regs(stdout);
 	return 0;
 }
 
-static reg_t x_exit(reg_t a)
+static reg_t op_exit(reg_t a)
 {
 	exit(a);
 }
 
-static reg_t hex(reg_t a)
+static reg_t op_hex(reg_t a)
 {
 	printf("%x\n", a);
 
 	return a;
 }
 
-static reg_t x_if(reg_t cond)
+static reg_t op_if(reg_t cond)
 {
 	parse_if();
 	return cond;
 }
 
-static reg_t mov(reg_t _, reg_t a)
+static reg_t op_mov(reg_t _, reg_t a)
 {
 	return a;
 }
 
-static reg_t or(reg_t _, reg_t a, reg_t b)
+static reg_t op_or(reg_t _, reg_t a, reg_t b)
 {
 	return a | b;
 }
 
-static reg_t print(reg_t a)
+static reg_t op_print(reg_t a)
 {
 	printf("%d\n", a);
 
 	return a;
 }
 
-static reg_t x_putc(reg_t a)
+static reg_t op_putc(reg_t a)
 {
 	putchar(a);
 
 	return a;
 }
 
-static reg_t shl(reg_t _, reg_t a, reg_t b)
+static reg_t op_shl(reg_t _, reg_t a, reg_t b)
 {
 	return a << b;
 }
 
-static reg_t shr(reg_t _, reg_t a, reg_t b)
+static reg_t op_shr(reg_t _, reg_t a, reg_t b)
 {
 	reg_t sb = (a >> 31) & 1;
 	reg_t partial = (~(1u << 31) & a) >> b;
@@ -99,7 +100,7 @@ static reg_t shr(reg_t _, reg_t a, reg_t b)
 	return partial | (sb << (31 - b));
 }
 
-static reg_t shra(reg_t _, reg_t a, reg_t b)
+static reg_t op_shra(reg_t _, reg_t a, reg_t b)
 {
 	reg_t sb = ((a >> 31) & 1) * -1u;
 	reg_t partial = (~(1u << 31) & a) >> b;
@@ -107,69 +108,62 @@ static reg_t shra(reg_t _, reg_t a, reg_t b)
 	return partial | (sb << (31 - b));
 }
 
-static reg_t sub(reg_t _, reg_t a, reg_t b)
+static reg_t op_sub(reg_t _, reg_t a, reg_t b)
 {
 	return a - b;
 }
 
-static reg_t x_while(reg_t cond)
+static reg_t op_while(reg_t cond)
 {
 	parse_while();
 	return cond;
 }
 
-static reg_t words(void)
+static reg_t op_words(void)
 {
 	symtab_list(stdout);
 	return 0;
 }
 
-static reg_t xor(reg_t _, reg_t a, reg_t b)
+static reg_t op_xor(reg_t _, reg_t a, reg_t b)
 {
 	return a ^ b;
 }
 
 void register_ops(void)
 {
-#define FN(x)                                               \
-	do {                                                \
-		static struct symbol s = { .name = #x,      \
-					   .type = FUNCPTR, \
-					   { .sym = &x } }; \
-		symtab_add(&s);                             \
-	} while (0)
-#define FX(x)                                                   \
+#define OP(x)                                                   \
 	do {                                                    \
 		static struct symbol s = { .name = #x,          \
 					   .type = FUNCPTR,     \
-					   { .sym = &x_##x } }; \
+					   { .sym = &op_##x } }; \
 		symtab_add(&s);                                 \
 	} while (0)
 #define IMM (symtab_latest()->type = WORDPTR)
 
-	FN(add);
-	FX(alloc);
-	FX(assert);
-	FN(and);
-	FN(define); IMM;
-	FX(disassemble); IMM;
-	FN(dump);
-	FX(exit);
-	FN(hex);
-	FX(if); IMM;
-	FN(mov);
-	FN(or);
-	FN(print);
-	FX(putc);
-	FN(shl);
-	FN(shr);
-	FN(shra);
-	FN(sub);
-	FX(while); IMM;
-	FN(words);
-	FN(xor);
+	OP(add);
+	OP(alloc);
+	OP(assert);
+	OP(and);
+	OP(define); IMM;
+	OP(disassemble); IMM;
+	OP(dump);
+	OP(exit);
+	OP(hex);
+	OP(if); IMM;
+	OP(mov);
+	OP(or);
+	OP(print);
+	OP(putc);
+	OP(shl);
+	OP(shr);
+	OP(shra);
+	OP(sub);
+	OP(while); IMM;
+	OP(words);
+	OP(xor);
+	OP(xor);
 
-#undef FN
-#undef FX
+#undef OP
 #undef IMM
 }
